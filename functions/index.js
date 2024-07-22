477b00b8ae62b46f01e1d370e38bc764deb1cfc2
Noirakumaaa <hernandezlnathaniel@gmail.com>
2024-07-22 22:49:35 +0800

    minor fix

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-const function = require('firebase-functions')
-
-export.randomNumber = function.https.onRequest((request, response) =>{
-    const num = Math.round(Math.round()*100)
-    response.send(Number.toString())
-})
