ae3790ca595e1cd9c1823692e1fcb04f49c37133
Noirakumaaa <hernandezlnathaniel@gmail.com>
2024-07-22 21:05:39 +0800

    latest

--- public/app.js
+++ public/app.js
@@ -1,26 +1,21 @@
-document.getElementById('openPopup').addEventListener('click', function() {
-    document.getElementById('emailPopup').style.display = 'flex';
-});
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
 
-document.getElementById('closePopup').addEventListener('click', function() {
-    document.getElementById('emailPopup').style.display = 'none';
-});
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-document.getElementById('emailForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA4UFp-VKqvbrWJ5a-_ym-002oNiAgp5HU",
+  authDomain: "myportfolio-e7689.firebaseapp.com",
+  databaseURL: "https://myportfolio-e7689-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "myportfolio-e7689",
+  storageBucket: "myportfolio-e7689.appspot.com",
+  messagingSenderId: "847564554",
+  appId: "1:847564554:web:f0ce118a21cb6249ccb4f3",
+  measurementId: "G-623C3KMSY8"
+};
 
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    console.log('Email:', email);
-    console.log('Message:', message);
-
-    alert('Your message has been sent!');
-
-    // Clear the form fields
-    document.getElementById('email').value = '';
-    document.getElementById('message').value = '';
-
-    // Close the popup
-    document.getElementById('emailPopup').style.display = 'none';
-});
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
