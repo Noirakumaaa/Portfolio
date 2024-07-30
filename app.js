2262976301b4394fbacbbaaf688ee0a55b90f8c6
Noirakumaaa <hernandezlnathaniel@gmail.com>
2024-07-30 19:02:01 +0800

    Setup express server for deploy

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + '/public/404.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
