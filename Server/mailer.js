68d4b97dc7a7043d1bc4134c065f24348201fc75
Noirakumaaa <hernandezlnathaniel@gmail.com>
2024-08-02 17:23:17 +0800

    fix some minor issues and add privacy to the email and pass

--- Server/mailer.js
+++ Server/mailer.js
@@ -4,15 +4,15 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'Hernandezlnathaniel@gmail.com', // Your Gmail address
-    pass: 'poes xtfw ifmn qkju'    // Your Gmail password or App password
+    user: process.env.EMAIL_USER, // Email address from environment variable
+    pass: process.env.EMAIL_PASS  // Email password or App password from environment variable
   }
 });
 
 // Function to send an email
 const sendEmail = (to, subject, text) => {
   const mailOptions = {
-    from: 'hernandezlnathaniel@gmail.com',
+    from: process.env.EMAIL_USER,
     to: to,
     subject: subject,
     text: text
